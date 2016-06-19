0ea961c18e5543987b9c7e03d3f4db259c93b258
arleyribeiro <arley.sribeiro@gmail.com>
2016-06-18 21:11:21 -0300

    Navegando node

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var flash = require('connect-flash');
 var methodOverride = require('method-override');
 var path = require('path');
 
+
 var routes = require('./routes/user');
 
 app.set('port', process.env.PORT || 3000);
@@ -30,10 +31,30 @@ app.use(session({
 
 app.use(flash());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/',function(req,res){
-  res.render('index.html');
+  res.render('index.hbs');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/index',function(req,res){
+  res.render('index.hbs');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/register',function(req,res){
+  res.render('register.hbs');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/login',function(req,res){
+  res.render('login.hbs');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/championships',function(req,res){
+  res.render('championships.hbs');
   //It will find and locate index.html from View or Scripts
 });
 
